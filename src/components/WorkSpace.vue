3eea8ad13ad627878ac8d4945a68436e28f9a355
ChaosHome <603352407@qq.com>
2021-03-14 06:12:15 +0800

    实验性引入绘图功能：2个cytoscape，3个d3

--- src/components/WorkSpace.vue
+++ src/components/WorkSpace.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="container">
-    <div class="graph-area">
-      知识图谱展示区
-    </div>
+<!--    <div class="graph-area">-->
+<!--      知识图谱展示区-->
+<!--    </div>-->
+    <d3-template></d3-template>
     <edit-bar class="edit-bar"></edit-bar>
   </div>
 </template>
 
 <script>
 import EditBar from "./EditBar";
+import d3Template from "./d3_ref1"
 
 export default {
   name: "WorkSpace",
-  components: {EditBar}
+  components: {EditBar,d3Template}
 }
 </script>
 
---
+++ src/views/d3.vue
@@ -0,0 +1,18 @@
+<!--<template>-->
+<!--    <div id="d3_worktop">-->
+<!--        <d3-template></d3-template>-->
+<!--    </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--    import d3Template from '../components/d3_ref1'-->
+
+<!--    export default {-->
+<!--        name: 'd3_worktop',-->
+<!--        components: {-->
+<!--            d3Template-->
+<!--        },-->
+<!--    }-->
+<!--</script>-->
+
+<!--<style></style>-->
