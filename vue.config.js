0a6f05c0c6d0fd7523bda2966ebe7d61fa2b17e7
Zhaoning <181250007@smail.nju.edu.cn>
2021-03-21 14:01:45 +0800

    feature: 接入远端服务器

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
                 //ws: true,                       //是否代理 websockets
                 //secure: false,                   //是否https接口
                 pathRewrite: {                  //路径重置
-                    '^/api': '/api'
+                    '^/api': '/demo/api'
                 }
             }
         },
