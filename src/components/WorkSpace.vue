159bb5e5b1113a6e3bd63b09881f554cf6a304f5
Zhaoning <181250007@smail.nju.edu.cn>
2021-04-12 11:46:34 +0800

    feature: 增加一键返回初始位置功能

--- src/components/WorkSpace.vue
+++ src/components/WorkSpace.vue
@@ -1,7 +1,30 @@
 <template>
-  <div class="container">
+  <div class="work-space-container">
+
     <kg-template class="kg-template"></kg-template>
+
     <edit-bar></edit-bar>
+
+    <div class="view-ops">
+      <el-tooltip effect="light" content="返回初始位置" placement="right" :enterable="false">
+        <div class="view-op" @click="back2Init">
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icondingwei"></use>
+          </svg>
+        </div>
+      </el-tooltip>
+      <div class="view-op" @mouseover="showChangeMode" @mouseout="showChangeMode">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#iconqiehuan1"></use>
+        </svg>
+        <div class="choose-mode-box" :class="{'collapsed':!ifShowChangeMode, 'expanded':ifShowChangeMode}">
+          <div @click="changeMode('force')" class="mode-op">力导图</div>
+          <div class="separator"></div>
+          <div @click="changeMode('typeset')" class="mode-op">排版</div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -14,8 +37,19 @@ import axios from "axios";
 export default {
   name: "WorkSpace",
   components: {EditBar,kgTemplate},
+  data(){
+    return {
+      ifShowChangeMode: false,
+      mode: 'force',
+      initZoom: 0,
+      initPan: {},
+    }
+  },
   computed: {
-    ...mapState(['current_pid']),
+    ...mapState({
+      current_pid: state => state.current_pid,
+      cy: state => state.workspace.cy,
+    }),
   },
   watch: {
     json_src_path(now, old) {
@@ -25,15 +59,117 @@ export default {
           })
           .catch(err => {})
     },
+    cy(newValue, oldValue){
+      this.initZoom = newValue._private.zoom;
+      let p_pan = this.cy._private.pan
+      this.initPan = {x: p_pan.x, y: p_pan.y};
+    }
+  },
+  methods: {
+    back2Init(){
+      this.cy.viewport({
+        zoom: this.initZoom,
+        pan: this.initPan,
+      })
+    },
+    showChangeMode(e){
+      this.ifShowChangeMode = ! this.ifShowChangeMode;
+    },
+    changeMode(mode){
+      let dom = document.getElementsByClassName('choose-mode-box')[0];
+      dom.classList.remove('expanded');
+      dom.classList.add('hide');
+      this.mode = mode;
+    }
   }
 }
 </script>
 
 <style scoped lang="less">
-.container{
+@import "../assets/css/colors.less";
+.work-space-container{
   height: 100%;
+  position: relative;
 }
 .kg-template{
   height: 100%;
+}
+.view-ops{
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  margin: 20px 15px;
+}
+.view-op{
+  position: relative;
+  height: 40px;
+  width: 40px;
+  border: 1px solid #e6e6e6;
+  background-color: rgba(255,255,255,0.8);
+  border-radius: 4px;
+  box-shadow: 0 0 10px #e6e6e6;
+  margin-top: 5px;
+}
+.view-op svg{
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: 70%;
+  height: 70%;
+}
+.view-op:hover{
+  cursor: pointer;
+  background-color: rgba(255,255,255,1);
+}
+
+@len3: 40px;
+.choose-mode-box{
+  background-color: white;
+  transform: translate3d(0,0,0);  //硬件加速
+  height: @len3;
+  border: 1px solid @separator;
+  border-radius: 4px;
+  border-left: none;
+  position: absolute;
+  left: @len3 + 3px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  z-index: 1;
+  .mode-op{
+    background-color: white;
+    height: @len3;
+    line-height: @len3;
+    width: @len3+20px;
+    text-align: center;
+    color: @prompt;
+    font-size: 13px;
+  }
+  .mode-op:hover{
+    background-color: #f0f7ff;
+    color: @theme;
+  }
+  overflow: hidden;
+}
+.collapsed {
+  transition: max-width 0.35s cubic-bezier(0, 1, 0, 1);
+  max-width: 0;
+  border: none;
+}
+.expanded {
+  max-width: 999px;
+  transition: max-width 0.35s cubic-bezier(0.75, 0, 0.9, 0);
+}
+.hide{
+  max-width: 0;
+  border-right: none;
+}
+.separator{
+  height: 20px;
+  width: 1px;
+  background-color: @separator;
 }
 </style>
