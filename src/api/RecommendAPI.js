55b7fd1976932e99f3b7744b02b3f2a9861118e3
Zhaoning <181250007@smail.nju.edu.cn>
2021-12-02 13:35:05 +0800

    fix: nodemodule 已加入

--- src/api/RecommendAPI.js
+++ src/api/RecommendAPI.js
@@ -5,9 +5,9 @@ export { getRecommend }
 
 let base =  '/demo/api/recommend/';
 
-function getRecommend(question){
+function getRecommend(question, pid){
     return new Promise((resolve,reject) => {
-        axios.get(base + 'simple_question' + '?question=' + question)
+        axios.get(base + 'simple_question' + '?question=' + question + '&pid=' + pid)
             .then(res => {
                 resolve(res.data);
             }).catch(error => {
